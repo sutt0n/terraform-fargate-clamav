56ce325e61ac82b20878a6cadb40f8b4e0f34e36
Joseph Sutton <jsutton@flexion.us>
2021-07-14 17:56:08 -0500

    consolidated s3 and sqs config into logistics.tf

--- terraform/logistics.tf
+++ terraform/logistics.tf
@@ -24,6 +24,21 @@ resource "aws_s3_bucket" "quarantine_bucket" {
   }
 }
 
+
+data "template_file" "event_queue_policy" {
+  template = file("templates/event-queue-policy.tpl.json")
+
+  vars = {
+    bucketArn = data.aws_s3_bucket.quarantine_bucket.arn
+  }
+}
+
+resource "aws_sqs_queue" "clamav_event_queue" {
+  name = "s3_clamav_event_queue"
+
+  policy = data.template_file.event_queue_policy.rendered
+}
+
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = data.aws_s3_bucket.quarantine_bucket.id
 
@@ -31,6 +46,10 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     queue_arn = aws_sqs_queue.clamav_event_queue.arn
     events    = ["s3:ObjectCreated:*"]
   }
+
+  depends_on = [
+    aws_sqs_queue.clamav_event_queue
+  ]
 }
 
 resource "aws_s3_bucket" "clean_bucket" {
